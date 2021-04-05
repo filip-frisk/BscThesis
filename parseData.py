804b576ce80a8c5ba42d65ccc5030d6dab55f03d
Filip Frisk <filipfri@alvis1.int.private>
2021-04-05 18:30:42 +0200

    changes to parseData, fixed w-bug by HA

--- parseData.py
+++ parseData.py
@@ -6,15 +6,17 @@ import xml.etree.ElementTree as ET
 # Cropping 32x32 images around the center of the cells
 # All images are stored in the array "images "and the labels are stored in array labels
 
-def parseData(basePath="KI-dataset-4-types/All_Slices/", filter_name= "", label_paths, class_names):
+def parseData(basePath="KI-dataset-4-types/All_Slices/", filter_name= "", label_paths="", class_names=[]):
     fileCount = len(label_paths)
     labels = []
     images = []
 
     for path in range(fileCount):
         # Parse xml tree
+        print(label_paths[path])
         tree = ET.parse(basePath+label_paths[path]+'.xml')
         root = tree.getroot()
+
 
         # Parse full image to nparray
         image = basePath+label_paths[path]+filter_name+'.tif'
@@ -34,10 +36,11 @@ def parseData(basePath="KI-dataset-4-types/All_Slices/", filter_name= "", label_
             # Parse label
             for name in child.iter('name'):
                 label = name.text
-                labels.append(class_names.index(label))
                 if label == 'w':
                     print(label_paths[path])
                     print(len(labels))
+                labels.append(class_names.index(label))
+
 
             # Parse matching image data
             for box in child.iter('bndbox'):
